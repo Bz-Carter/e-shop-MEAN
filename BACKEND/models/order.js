bc6e926439f6917561d5ea0abb2c7a20670a1240
Bz-Carter <lebz2016@gmail.com>
2021-08-19 09:21:27 +0000

    backend: created order

--- BACKEND/models/order.js
+++ BACKEND/models/order.js
@@ -1,7 +1,85 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
-
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required:true
+    }],
+    shippingAddress1: {
+        type: String,
+        required: true,
+    },
+    shippingAddress2: {
+        type: String,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    zip: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'Pending',
+    },
+    totalPrice: {
+        type: Number,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    dateOrdered: {
+        type: Date,
+        default: Date.now,
+    },
 })
 
+orderSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+orderSchema.set('toJSON', {
+    virtuals: true,
+});
+
 exports.Order = mongoose.model('Order', orderSchema);
+
+
+
+/**
+Order Example:
+
+{
+    "orderItems" : [
+        {
+            "quantity": 5,
+            "product" : "5f15da13e520d44421ed8e9e"
+        },
+        {
+            "quantity": 4,
+            "product" : "5f15da13e520d44421ed8e9e"
+        }
+    ],
+    "shippingAddress1" : "Flowers Street , 45",
+    "shippingAddress2" : "1-B",
+    "city": "Prague",
+    "zip": "00000",
+    "country": "Czech Republic",
+    "phone": "+420702241333",
+    "user": "609d65943373711346c5e950"
+}
+
+ */
