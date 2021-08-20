f1f795317f1c5c02e0c1cc952ff46b5d62b32a20
Bz-Carter <lebz2016@gmail.com>
2021-08-20 09:43:36 +0000

    frontend: basic structure for E-shop admin panel

---
+++ FRONTEND/tel-shop/apps/admin/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'admin-dashoard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ FRONTEND/tel-shop/libs/ui/README.md
@@ -0,0 +1,11 @@
+# ui
+
+This library containes the ui component which are use in the Tel Shop corporate
+
+# Banner
+# Slider
+# Stepper
+
+## Running unit tests
+
+Run `nx test ui` to execute the unit tests via [Jest](https://jestjs.io).
